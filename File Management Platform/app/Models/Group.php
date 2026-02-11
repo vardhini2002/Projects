0a4c46563b1f14df4974c4f3ca5c6c6bc8058301
vardhini2002 <harshavardhini0409@gmail.com>
2026-02-11 21:41:51 +0530

    changes

--- File Management Platform/app/Models/Group.php
+++ File Management Platform/app/Models/Group.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
-
 class Group extends Model
 {
     use HasFactory;
