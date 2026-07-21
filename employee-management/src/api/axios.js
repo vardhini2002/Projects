5ec3370892a7fc6a0ef3188acfec567f96cec8e2
greentcube <green.coder@tcube.tech>
2026-07-21 16:58:59 +0530

    added fake backend "json - server"

--- employee-management/src/api/axios.js
+++ employee-management/src/api/axios.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api= axios.create({
-    baseURL: 'http://localhost:5173/api',
-    headers:{
-        'Content-Type': 'application/json'
+const api = axios.create({
+    baseURL: "http://localhost:3000",
+    headers: {
+        "Content-Type": "application/json"
     }
-
 });
+
 export default api;
